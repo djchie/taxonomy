93ff9a97e063f8faf79f2b0064dfd6dcb7b96359
Derrick J Chie <djchie.dev@gmail.com>
2015-08-26 11:08:36 -0700

    Implemented the Bee class

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,13 @@
 var Bee = function() {
+  // Constructs a Grub to inherit properties
+  Grub.call(this);
+  // Adding on properties to Bee
+  this.age = 5;
+  this.color = "yellow";
+  this.job = "keep on growing";
 };
 
+// Inherit Grub's prototype
+Bee.prototype = Object.create(Grub.prototype);
+// Override the constructor with value "Grub" to have "Bee"
+Bee.prototype.constructor = Bee;
