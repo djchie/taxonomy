519b6f46f12ebcdfa47d12cc7b259503a4000def
Derrick J Chie <djchie.dev@gmail.com>
2015-08-26 11:10:01 -0700

    Implemented the HoneyMakerBee class inherited from the Bee class

--- src/HoneyMakerBee.js
+++ src/HoneyMakerBee.js
@@ -1,3 +1,23 @@
 var HoneyMakerBee = function() {
+  // Constructs a Bee to inherit properties
+  Bee.call(this);
+  // Adding on properties to HoneyMakerBee
+  this.age = 10;
+  this.job = "make honey";
+  this.honeyPot = 0;
 };
 
+// Inherit Bee's prototype
+HoneyMakerBee.prototype = Object.create(Bee.prototype);
+// Override the constructor with value "Bee" to have "HoneyMakerBee"
+HoneyMakerBee.prototype.constructor = HoneyMakerBee;
+
+// Adding on the makeHoney method to HoneyMakerBee's prototype
+HoneyMakerBee.prototype.makeHoney = function() {
+  this.honeyPot++;
+};
+
+// Adding on the giveHoney method to HoneyMakerBee's prototype
+HoneyMakerBee.prototype.giveHoney = function() {
+  this.honeyPot--;
+};
