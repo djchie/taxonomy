2a691f5cf1d829e1d24c6d724a858635abe2c43a
Derrick J Chie <djchie.dev@gmail.com>
2015-08-26 11:09:44 -0700

    Implemented the Grub class

--- src/Grub.js
+++ src/Grub.js
@@ -1,3 +1,12 @@
 var Grub = function() {
+  // Setting properties for Grub
+  this.age = 0;
+  this.color = "pink";
+  this.food = "jelly";
+};
+
+// Adding on the eat method to Grub's prototype
+Grub.prototype.eat = function() {
+
 };
 
