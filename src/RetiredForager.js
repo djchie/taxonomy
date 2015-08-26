5b7f4b370eb5c85a33c3e6f85b53d25314b11dce
Derrick J Chie <djchie.dev@gmail.com>
2015-08-26 11:10:48 -0700

    Implemented the RetiredForagerBee class inherited from the ForagerBee class

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,24 @@
 var RetiredForagerBee = function() {
+  // Constructs a ForagerBee to inherit properties
+  ForagerBee.call(this);
+  // Adding on properties to RetiredForagerBee
+  this.age = 40;
+  this.job = "gamble";
+  this.canFly = false;
+  this.color = "grey";
 };
 
+// Inherit ForagerBee's prototype
+RetiredForagerBee.prototype = Object.create(ForagerBee.prototype);
+// Override the constructor with value "ForagerBee" to have "RetiredForagerBee"
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
+
+// Adding on the forage method to HoneyMakerBee's prototype
+RetiredForagerBee.prototype.forage = function() {
+  return "I am too old, let me play cards instead";
+}
+
+// Adding on the gamble method to HoneyMakerBee's prototype
+RetiredForagerBee.prototype.gamble = function(treasure) {
+  this.treasureChest.push(treasure);
+}
