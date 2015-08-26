d96a8f1adf875365e50a870a9f63094d0d502639
Derrick J Chie <djchie.dev@gmail.com>
2015-08-26 11:09:02 -0700

    Implemented the Bee class inherited from the Grub class

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,19 @@
 var ForagerBee = function() {
+  // Constructs a Bee to inherit properties
+  Bee.call(this);
+  // Adding on properties to ForagerBee
+  this.age = 10;
+  this.job = "find pollen";
+  this.canFly = true;
+  this.treasureChest = [];
 };
 
+// Inherit Bee's prototype
+ForagerBee.prototype = Object.create(Bee.prototype);
+// Override the constructor with value "Bee" to have "ForagerBee"
+ForagerBee.prototype.constructor = ForagerBee;
+
+// Adding on the forage method to HoneyMakerBee's prototype
+ForagerBee.prototype.forage = function(treasure) {
+  this.treasureChest.push(treasure);
+}
